06b62bd66d25be4db7d82036c9721b487cfd0286
NelsonZabala <nelsonzabalacba@gmail.com>
2019-08-01 23:50:21 -0300

    errornew test

--- src/Container.php
+++ src/Container.php
@@ -60,7 +60,7 @@ class Container
      */
     public function singleton($name, $resolver)
     {
-        $this->bindin($name, $resolver, true);
+        $this->bindinses($name, $resolver, true);
     }
 
     public function make($name, array $arguments = array())
